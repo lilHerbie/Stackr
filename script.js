03ab64485ab0fdf527da3f267c20d0ec8a43d733
lilHerbie <ht.alting@student.avans.nl>
2023-05-05 13:26:14 +0200

    Setup

---
+++ script.js
@@ -0,0 +1,19 @@
+class Truck{
+    constructor(transportType, length, width, interval){
+        this.transportType = transportType;
+        this.length = length;
+        this.width = width;
+        this.interval = interval;
+    }
+}
+
+
+const TransportTypes = {
+    ColdTransport : "Cold tranport",
+    FragileTransport : "Fragile transport",
+    GeneralTransport : "General transport",
+    Pallets : "Pallets",
+    FastTransport : "Fast transport"
+}
+
+
