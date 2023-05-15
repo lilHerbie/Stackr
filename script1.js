0c8a47a098ad8611ffc2f12f71b773d0b6d52c6c
Enyuuus <70026184+Enyuuus@users.noreply.github.com>
2023-05-15 21:42:58 +0200

    truk

--- script1.js
+++ script1.js
@@ -1,4 +1,4 @@
-const apiKey = 'demo'
+const apiKey = 'e1d4681ba6';
 
 function getWeer(stat){
     let apiUrl = `https://weerlive.nl/api/json-data-10min.php?key=${apiKey}&locatie=${stat}`;
@@ -12,7 +12,17 @@ function getWeer(stat){
     });
 }
 
-getWeer("Amsterdam").then((weer) => {
-    console.log(weer);
-    // doe hier iets met de weerdata
-  });
+// getWeer("Amsterdam").then((weer) => {
+//     console.log(weer);
+//     // doe hier iets met de weerdata
+// });
+
+
+// t = new Truck("Cold tranport", 10,10,5);
+// t.show();
+// t.place(2,1,[['bezet','bezet','bezet','bezet'],['plaats','plaats','plaats','plaats'],['plaats','plaats','plaats','plaats'],['plaats','plaats','plaats','plaats']]);
+// t.show();
+// t.place(3,1,[['bezet','plaats','plaats','plaats'],['bezet','plaats','plaats','plaats'],['bezet','plaats','plaats','plaats'],['bezet','plaats','plaats','plaats']]);
+
+// t.show();
+
