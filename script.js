95c0d8a019e41216aec501ffed7086c6f9f76e45
lilHerbie <ht.alting@student.avans.nl>
2023-05-15 20:44:36 +0200

    AssemblyLine

--- script.js
+++ script.js
@@ -16,9 +16,50 @@ let truckForm = document.getElementById('TruckForm')
 let assemblyForm = document.getElementById('AssemblyForm')
 let locationForm = document.getElementById('LocationForm')
 
+//costum elements
+
+
+
+class Truck extends HTMLElement{
+    constructor(transportType, length, width, interval) {
+        super();
+        this.transportType = transportType;
+        this.length = length;
+        this.width = width;
+        this.interval = interval;
+    }
+
+    connectedCallback() {
+        this.innerHTML = 
+        `<div class="Truck">
+            <img src="Assets/truck.png">
+        </div>`
+    }
+}
+
+class AssemblyLine extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML =
+        `<div class="AssemblyLineContainer">
+            <div class="AssemblyLine">
+                <p>lopende band</p>
+            </div>
+            <div class="TruckContainer">
+
+            </div>
+        </div>`
+    }
+}
+
+customElements.define("assembly-line", AssemblyLine);
+
 //functions
 
-function showForm(form){
+function showForm(form) {
     formContainer.style.display = formContainer.style.display === 'flex' ? 'none' : 'flex';
     truckForm.style.display = 'none';
     assemblyForm.style.display = 'none';
@@ -26,23 +67,24 @@ function showForm(form){
     form.style.display = 'flex';
 }
 
-
-function addAssemblyLine(){
-    //TODO build functionality
+function addAssemblyLine() {
+    let assemblyline = new AssemblyLine();
+    hallContainer.appendChild(assemblyline);
 }
-
 
 //eventlisteners
 
-addTruckButton.addEventListener('click', function() {
+addTruckButton.addEventListener('click', function () {
     showForm(truckForm);
 });
 
-assmblylineSettingsButton.addEventListener('click', function() {
+addAssemblylineButton.addEventListener('click', addAssemblyLine);
+
+assmblylineSettingsButton.addEventListener('click', function () {
     showForm(assemblyForm);
 });
 
-changeLocationButton.addEventListener('click', function() {
+changeLocationButton.addEventListener('click', function () {
     showForm(locationForm);
 });
 
@@ -52,24 +94,17 @@ let truckFormButton = document.getElementById('SubmitTruck');
 
 let Trucks = [];
 
-class Truck{
-    constructor(transportType, length, width, interval){
-        this.transportType = transportType;
-        this.length = length;
-        this.width = width;
-        this.interval = interval;
-    }
-}
+
 
 const TransportTypes = {
-    ColdTransport : "Cold tranport",
-    FragileTransport : "Fragile transport",
-    GeneralTransport : "General transport",
-    Pallets : "Pallets",
-    FastTransport : "Fast transport"
+    ColdTransport: "Cold tranport",
+    FragileTransport: "Fragile transport",
+    GeneralTransport: "General transport",
+    Pallets: "Pallets",
+    FastTransport: "Fast transport"
 }
 
-function submitForm(){
+function submitForm() {
     //TODO add form functionality
     truckFormButton.innerHTML = "verstuurd";
 }
